0289ebb36121135f7afde54161143a8c4e5de74a
Roads-To-The-Doors <dimacernovolov@gmail.com>
2022-07-26 02:17:46 +0300

    Добавьте файлы проекта.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace RockScissorsPaper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.butRock = new System.Windows.Forms.Button();
+            this.butScissors = new System.Windows.Forms.Button();
+            this.butPaper = new System.Windows.Forms.Button();
+            this.cntUser = new System.Windows.Forms.TextBox();
+            this.cntPC = new System.Windows.Forms.TextBox();
+            this.UChoice = new System.Windows.Forms.TextBox();
+            this.PChoice = new System.Windows.Forms.TextBox();
+            this.AllResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // butRock
+            // 
+            this.butRock.BackColor = System.Drawing.Color.White;
+            this.butRock.BackgroundImage = global::RockScissorsPaper.Properties.Resources.Камень;
+            this.butRock.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.butRock.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.butRock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butRock.Location = new System.Drawing.Point(14, 120);
+            this.butRock.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.butRock.Name = "butRock";
+            this.butRock.Size = new System.Drawing.Size(100, 100);
+            this.butRock.TabIndex = 0;
+            this.butRock.TabStop = false;
+            this.butRock.UseVisualStyleBackColor = false;
+            this.butRock.Click += new System.EventHandler(this.butRock_Click);
+            // 
+            // butScissors
+            // 
+            this.butScissors.BackColor = System.Drawing.Color.White;
+            this.butScissors.BackgroundImage = global::RockScissorsPaper.Properties.Resources.Ножницы;
+            this.butScissors.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.butScissors.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.butScissors.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butScissors.Location = new System.Drawing.Point(122, 120);
+            this.butScissors.Name = "butScissors";
+            this.butScissors.Size = new System.Drawing.Size(100, 100);
+            this.butScissors.TabIndex = 1;
+            this.butScissors.TabStop = false;
+            this.butScissors.UseVisualStyleBackColor = false;
+            this.butScissors.Click += new System.EventHandler(this.butScissors_Click);
+            // 
+            // butPaper
+            // 
+            this.butPaper.BackColor = System.Drawing.Color.White;
+            this.butPaper.BackgroundImage = global::RockScissorsPaper.Properties.Resources.Бумага;
+            this.butPaper.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.butPaper.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.butPaper.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.butPaper.Location = new System.Drawing.Point(228, 120);
+            this.butPaper.Name = "butPaper";
+            this.butPaper.Size = new System.Drawing.Size(100, 100);
+            this.butPaper.TabIndex = 2;
+            this.butPaper.TabStop = false;
+            this.butPaper.UseVisualStyleBackColor = false;
+            this.butPaper.Click += new System.EventHandler(this.butPaper_Click);
+            // 
+            // cntUser
+            // 
+            this.cntUser.BackColor = System.Drawing.Color.White;
+            this.cntUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cntUser.Location = new System.Drawing.Point(12, 12);
+            this.cntUser.Name = "cntUser";
+            this.cntUser.ReadOnly = true;
+            this.cntUser.ShortcutsEnabled = false;
+            this.cntUser.Size = new System.Drawing.Size(160, 30);
+            this.cntUser.TabIndex = 3;
+            this.cntUser.TabStop = false;
+            this.cntUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cntPC
+            // 
+            this.cntPC.BackColor = System.Drawing.Color.White;
+            this.cntPC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cntPC.Location = new System.Drawing.Point(171, 12);
+            this.cntPC.Name = "cntPC";
+            this.cntPC.ReadOnly = true;
+            this.cntPC.ShortcutsEnabled = false;
+            this.cntPC.Size = new System.Drawing.Size(160, 30);
+            this.cntPC.TabIndex = 4;
+            this.cntPC.TabStop = false;
+            this.cntPC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // UChoice
+            // 
+            this.UChoice.BackColor = System.Drawing.Color.White;
+            this.UChoice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UChoice.Location = new System.Drawing.Point(12, 48);
+            this.UChoice.Name = "UChoice";
+            this.UChoice.ReadOnly = true;
+            this.UChoice.ShortcutsEnabled = false;
+            this.UChoice.Size = new System.Drawing.Size(160, 30);
+            this.UChoice.TabIndex = 5;
+            this.UChoice.TabStop = false;
+            this.UChoice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PChoice
+            // 
+            this.PChoice.BackColor = System.Drawing.Color.White;
+            this.PChoice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PChoice.Location = new System.Drawing.Point(171, 48);
+            this.PChoice.Name = "PChoice";
+            this.PChoice.ReadOnly = true;
+            this.PChoice.ShortcutsEnabled = false;
+            this.PChoice.Size = new System.Drawing.Size(160, 30);
+            this.PChoice.TabIndex = 6;
+            this.PChoice.TabStop = false;
+            this.PChoice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AllResult
+            // 
+            this.AllResult.BackColor = System.Drawing.Color.White;
+            this.AllResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AllResult.Location = new System.Drawing.Point(12, 84);
+            this.AllResult.Name = "AllResult";
+            this.AllResult.ReadOnly = true;
+            this.AllResult.ShortcutsEnabled = false;
+            this.AllResult.Size = new System.Drawing.Size(319, 30);
+            this.AllResult.TabIndex = 7;
+            this.AllResult.TabStop = false;
+            this.AllResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(343, 234);
+            this.Controls.Add(this.AllResult);
+            this.Controls.Add(this.PChoice);
+            this.Controls.Add(this.UChoice);
+            this.Controls.Add(this.cntPC);
+            this.Controls.Add(this.cntUser);
+            this.Controls.Add(this.butPaper);
+            this.Controls.Add(this.butScissors);
+            this.Controls.Add(this.butRock);
+            this.Font = new System.Drawing.Font("Lucida Console", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Rock-Scissors-Paper";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butRock;
+        private System.Windows.Forms.Button butScissors;
+        private System.Windows.Forms.Button butPaper;
+        private System.Windows.Forms.TextBox cntUser;
+        private System.Windows.Forms.TextBox cntPC;
+        private System.Windows.Forms.TextBox UChoice;
+        private System.Windows.Forms.TextBox PChoice;
+        private System.Windows.Forms.TextBox AllResult;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockScissorsPaper
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        int countUserWins = 0, countPCWins = 0;
+
+        string result, beenResult, userChoice, PCChoice;
+
+        string[] elements = 
+        {
+            "scissors", "rock", "paper"
+        };
+
+        string[] statuses =
+        {
+            " <--   draw   --> ", " -->   win   --> ", " <--   win   <-- "
+        };
+
+        private int Rand()
+        {
+            Random random = new Random();
+
+            int c;
+
+            int[] r1 = new int[10];
+
+            for (int i = 0; i < r1.Length; ++i)
+            {
+                r1[i] = random.Next(elements.Length);
+            }
+
+            int[] r2 = new int[5];
+
+            for (int i = 0; i < r2.Length; ++i)
+            {
+                c = random.Next(r1.Length);
+
+                r2[i] = r1[c];
+            }
+
+            int[] r3 = new int[3];
+
+            for (int i = 0; i < r3.Length; ++i)
+            {
+                c = random.Next(r2.Length);
+
+                r3[i] = r2[c];
+            }
+
+            c = random.Next(r3.Length);
+
+            return r3[c];
+        }
+
+        private string GetRandomElement()
+        {
+            Random random = new Random();
+
+            int rand;
+
+            string element;
+
+            rand = Rand();
+
+            element = elements[rand];
+
+            /*
+            do
+            {
+                rand = Rand();
+
+                element = elements[rand];
+            }
+            while (element == beenResult);
+            */
+
+            return element;
+        }
+
+        private string NextElement(string elem)
+        {
+            int length = elements.Length;
+
+            for (int i = 0; i < length; ++i)
+            {
+                if (elem == elements[i])
+                {
+                    if (i == length - 1)
+                    {
+                        return elements[0];
+                    }
+                    else
+                    {
+                        return elements[i + 1];
+                    }
+                }
+            }
+
+            return "error";
+        }
+
+        private string GetResults()
+        {
+            // если руки одинаковы
+            if (userChoice == PCChoice)
+            {
+                return statuses[0];
+            }
+            // если у пк рука сильней
+            if (PCChoice == NextElement(userChoice))
+            {
+                return statuses[1];
+            }
+            // если у нас рука сильней
+            else
+            {
+                return statuses[2];
+            }
+        }
+
+        private void CheckResult()
+        {
+            if (result == statuses[1])
+            {
+                ++countPCWins;
+            }
+            if (result == statuses[2])
+            {
+                ++countUserWins;
+            }
+        }
+
+        private void SetResults()
+        {
+            cntUser.Text = Convert.ToString(countUserWins);
+
+            cntPC.Text = Convert.ToString(countPCWins);
+        }
+
+        public void Game()
+        {
+            UChoice.Text = userChoice;
+
+            PCChoice = GetRandomElement();
+
+            beenResult = PCChoice;
+
+            PChoice.Text = PCChoice;
+
+            result = GetResults();
+
+            AllResult.Text = result;
+
+            CheckResult();
+
+            SetResults();
+        }
+
+        private void butRock_Click(object sender, EventArgs e)
+        {
+            userChoice = elements[1];
+
+            Game();
+        }
+
+        private void butScissors_Click(object sender, EventArgs e)
+        {
+            userChoice = elements[0];
+
+            Game();
+        }
+
+        private void butPaper_Click(object sender, EventArgs e)
+        {
+            userChoice = elements[2];
+
+            Game();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SetResults();
+        }
+    }
+}
